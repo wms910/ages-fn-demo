f768c6032b2336f17a00feea3d57aef2aed8e3df
Ming Wu <54914852+wms910@users.noreply.github.com>
2020-04-14 19:03:59 -0500

    Revert "Removing test file to demo PR"

--- Assets/Scripts/Interfaces/IInteractive.cs
+++
@@ -1,8 +0,0 @@
-﻿/// <summary>
-/// Interface for elements the playeyer can interact with by pressing the Interact button.
-/// </summary>
-public interface IInteractive 
-{
-    string DisplayText { get; }
-    void InteractWith();
-}
