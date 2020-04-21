d8d63be098ce2f94914a9fc639243a230e190ef6
Ming Wu <ming.wu@loop.colum.edu>
2020-04-21 18:00:46 -0500

    part 3 done

---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,69 @@
+﻿using EasySurvivalScripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour
+{
+    private static InventoryMenu instance;
+    private CanvasGroup canvasGroup;
+    private PlayerCamera playercamera;
+    private PlayerMovement playermovement;
+    public static InventoryMenu Instance
+    {
+        get
+        {
+            if (instance == null)
+                throw new System.Exception("There is currently no InventoryMenu instance, but you are trying to access it! Make sure the InventoryMenu script is applied to a GameObject in your scene!");
+            return instance;
+        }
+        private set { instance = value; }
+    }
+
+    private bool IsVisible => canvasGroup.alpha > 0;
+
+    private void ShowMenu()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        playercamera.enabled = false;
+        playermovement.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void HideMenu()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        playercamera.enabled = true;
+        playermovement.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Show Inventory Menu"))
+            if (IsVisible)
+                HideMenu();
+            else
+                ShowMenu();
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            throw new System.Exception("There is already an instance of InventoryMenu and there can only be one.");
+
+        canvasGroup = GetComponent<CanvasGroup>();
+        playercamera = FindObjectOfType<PlayerCamera>();
+        playermovement = FindObjectOfType<PlayerMovement>();
+    }
+    private void Start()
+    {
+        HideMenu();
+    }
+}
--- Assets/Scripts/InventoryObject.cs
+++ Assets/Scripts/InventoryObject.cs
@@ -45,5 +45,6 @@ public class InventoryObject : InteractiveObject
         PlayerInventory.InventoryObjects.Add(this);
         renderer.enabled = false;
         collider.enabled = false;
+        Debug.Log($"Inventory menu game object name {InventoryMenu.Instance.name}");
     }
 }
